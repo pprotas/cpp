3a52e552e120d4b47b8f52f996ed5bda6fe835de
Pawel Protas <pawprotas@gmail.com>
2018-11-20 14:53:23 +0100

    Mandelbrot multi-threading

--- CP/mandelbrot.cpp
+++ CP/mandelbrot.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <iostream>
 #include <complex> // if you make use of complex number facilities in C++
+#include <thread>
+#include <vector>
 
 template <class T>
 struct RGB
@@ -73,54 +75,93 @@ class PPMImage : public Matrix<RGB<unsigned char>>
     }
 };
 
-const void calculateImage(PPMImage &image, const unsigned &width, const unsigned &height)
+const void calculateImage(PPMImage &image, unsigned &width, unsigned &height, unsigned minX, unsigned maxX, unsigned minY, unsigned maxY)
 {
     double MinRe = -2.0;
     double MaxRe = 1.0;
     double MinIm = -1.2;
-    double MaxIm = MinIm + (MaxRe - MinRe) * height / width;
+    double MaxIm = MinIm + (MaxRe - MinRe) * width / height;
     double Re_facor = (MaxRe - MinRe) / (width - 1);
     double Im_factor = (MaxIm - MinIm) / (height - 1);
     unsigned MaxIterations = 30;
 
-    std::cout << "Calculating...\n";
-    for (unsigned y = 0; y < height; ++y)
+    for (unsigned y = minY; y < maxY; ++y)
     {
         double c_im = MaxIm - y * Im_factor;
-        for (unsigned x = 0; x < width; ++x)
+        for (unsigned x = minX; x < maxX; ++x)
         {
-            double c_re = MinRe + x*Re_facor;
+            double c_re = MinRe + x * Re_facor;
 
             double Z_re = c_re, Z_im = c_im;
             bool isInside = true;
-            for(unsigned n = 0; n < MaxIterations;++n){
-                double Z_re2 = Z_re*Z_re, Z_im2 = Z_im*Z_im;
-                if(Z_re2 + Z_im2 > 4){
+            for (unsigned n = 0; n < MaxIterations; ++n)
+            {
+                double Z_re2 = Z_re * Z_re, Z_im2 = Z_im * Z_im;
+                if (Z_re2 + Z_im2 > 4)
+                {
                     isInside = false;
                     break;
                 }
-                Z_im = 2 * Z_re *Z_im + c_im;
-                Z_re = Z_re2-Z_im2 + c_re;
+                Z_im = 2 * Z_re * Z_im + c_im;
+                Z_re = Z_re2 - Z_im2 + c_re;
             }
-            if(isInside){
+            if (isInside)
+            {
                 image[y][x].r = 255;
                 image[y][x].g = 0;
                 image[y][x].b = 0;
             }
         }
     }
-    std::cout << "Done.\n";
 }
 
 int main()
 {
-    const unsigned width = 100000;
-    const unsigned height = 100000;
+    unsigned width = 1600;
+    unsigned height = 1600;
+    unsigned minX = 0;
+    unsigned minY = 0;
+    unsigned maxX = width / 2;
+    unsigned maxY = height / 2;
 
+    std::cout << "Creating image...\n";
     PPMImage image(height, width);
 
-    calculateImage(image, width, height);
+    std::vector<std::thread> workers;
+    for (int i = 0; i < 4; i++)
+    {
+        workers.push_back(std::thread(calculateImage, std::ref(image), std::ref(width), std::ref(height), minX, maxX, minY, maxY));
+        std::cout << "Thread #" << i << " created. Calculating (" << minX << "," << minY << ") to (" << maxX << "," << maxY << ").\n";
 
+        if (i == 0)
+        {
+            minY = height / 2;
+            maxY = height;
+        }
+        else if (i == 1)
+        {
+            minX = width / 2;
+            maxX = width;
+            minY = 0;
+            maxY = height / 2;
+        }
+        else if (i == 2)
+        {
+            minX = width / 2;
+            maxX = width;
+            minY = height / 2;
+            maxY = height;
+        }
+    }
+
+    for (auto &th : workers)
+    {
+        th.join();
+    }
+
+    std::cout << "Saving to image...\n";
     image.save("mandelbrot.ppm");
+
+    std::cout << "Finished.\n";
     return 0;
 }
